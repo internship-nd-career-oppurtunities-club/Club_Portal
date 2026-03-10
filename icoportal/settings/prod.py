1233b0e5fd21eff9f9122e0d65b90732b09f31c3
royallearner <royallearner>
2026-03-10 08:34:42 +0530

    modified:   settings/prod.py for testing commit

--- icoportal/settings/prod.py
+++ icoportal/settings/prod.py
@@ -86,12 +86,7 @@ WSGI_APPLICATION = 'icoportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 # Database
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+
 
 
 # Password validation
